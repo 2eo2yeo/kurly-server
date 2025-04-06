c00c4ab9cba0137d1a1bc06acc5aca220eb4c436
2eo2yeo <2eo2yeo@gmail.com>
2025-04-06 20:56:34 +0900

    db수정3

--- repository/db.js
+++ repository/db.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2';
 const pool = mysql.createPool({
     host: 'kurly-pj-db.cjw8casuq6rz.ap-northeast-2.rds.amazonaws.com',
     port: 3306,
-    user: 'root',
+    user: 'admin',
     password: 'zjfflzmffhs1234**',
     database: 'kurlydb',
 });
