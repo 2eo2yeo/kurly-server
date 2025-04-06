c8c60eb6420772a6058570c1fabeb9555d52d977
2eo2yeo <2eo2yeo@gmail.com>
2025-04-06 20:47:06 +0900

    db 수정

--- repository/db.js
+++ repository/db.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2';
 
 const pool = mysql.createPool({
-    host: 'localhost',
+    host: 'http://kurly-pj-front.s3-website.ap-northeast-2.amazonaws.com/',
     port: 3306,
     user: 'root',
     password: 'zjfflzmffhs1234**',
